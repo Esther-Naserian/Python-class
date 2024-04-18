6731ee1b10d61f98f73be28d1ff49dd21916c3c2
Esther-Naserian <naseriansamuria@gmail.com>
2024-04-18 19:29:30 +0300

    add files

---
+++ Hello.py
@@ -0,0 +1,12 @@
+def hello():
+    print("Hello AkiraChix")
+
+def hello(name):
+    print(f"Hello {name}")
+def year_of_birth(name,age):
+    print(f"Hello {name} you have born in{2024-age}")
+
+def my_country(country="uganda"):
+    print(f"Hello AkiraChix from {country}")
+    
+ 
---
+++ calculator.py
@@ -0,0 +1,25 @@
+def add(x,y):
+    result=x+y
+    return result
+
+  def subtract(x,y):
+      result=x-y  
+      return result
+
+def divide(x,y):
+    result=x/y
+    return result
+
+def multiply(x,y):
+    result=x*y
+    return result
+
+def remainder(x,y):
+    result=x%y
+    return result
+
+def power of(x,y) 
+    result=x**y 
+    return result
+
+
